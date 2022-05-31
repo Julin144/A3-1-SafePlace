b35238fd1bb879d6ca0ee829fc9398d2b3dd86cc
Thais <53150263+TDhyx@users.noreply.github.com>
2022-05-31 20:58:22 -0300

    Home e telas

--- SafePlace/build/classes/Utils/SafePlace.sql
+++ SafePlace/build/classes/Utils/SafePlace.sql
@@ -15,6 +15,18 @@ CREATE TABLE Condominio(
     nome VARCHAR(100),
     endereco VARCHAR(200)
 );
+
+INSERT INTO Condominio(nome,endereco)(?,?);
+
+UPDATE  Condominio 
+SET     nome= ?,
+        endereco= ?,
+WHERE   idCondominio = x;
+
+DELETE *
+FROM   Condominio
+WHERE  idCondominio = x;
+
 #DROP TABLE Usuario;
 CREATE TABLE Usuario(
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -22,18 +34,44 @@ CREATE TABLE Usuario(
     senha VARCHAR(50),
     tipo  VARCHAR(20)
 );
+
+INSERT INTO Usuario(nome,senha,tipo)(?,?,?);
+
 #DROP TABLE Area;
 CREATE TABLE Area(
     idArea INT PRIMARY KEY AUTO_INCREMENT,
     descricao VARCHAR(200),
     dosesRequisitadas INT
 );
+INSERT INTO Area(descriçao,dosesRequisitadas)(?,?);
+
+UPDATE Area 
+SET     descriçao = ?,
+        dosesRequisitadas= ?,
+WHERE   idArea = x;
+
+DELETE *
+FROM   Area
+WHERE  idArea = x;
+
 #DROP TABLE Vacina;
 CREATE TABLE Vacina(
     idVacina INT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(19),
     qtdDose INT
 );
+
+INSERT INTO Vacina(tipo,qtdDose)(?,?);
+
+UPDATE Vacina 
+SET     tipo = ?,
+        qtdDose= ?,
+WHERE   idVacina = x;
+
+DELETE *
+FROM   Vacina
+WHERE  idVacina = x;
+
 #DROP TABLE Inquilino;
 CREATE TABLE Inquilino(
     idInquilino INT PRIMARY KEY AUTO_INCREMENT,
@@ -41,14 +79,26 @@ CREATE TABLE Inquilino(
     cpf VARCHAR(14),
     aprtNumero INT
 );
-#DROP TABLE InquilinoVacinado;
-CREATE TABLE InquilinoVacinado(
-    idVacinado INT PRIMARY KEY AUTO_INCREMENT,
-    idInquilino INT,
-    idVacina INT,
-    FOREIGN KEY (idInquilino) REFERENCES inquilino (idInquilino),
-    FOREIGN KEY (idVacina) REFERENCES Vacina (idVacina)
-);
+INSERT INTO Inquilino(tipo,qtdDose)(?,?);
+
+UPDATE Inquilino 
+SET     nome = ?,
+        cpf= ?,
+        aprtNumero= ?,
+WHERE   idInquilino = x;
+
+DELETE *
+FROM   Inquilino
+WHERE  idInquilino = x;
+--#DROP TABLE InquilinoVacinado
+--CREATE TABLE InquilinoVacinado(
+--    idVacinado INT PRIMARY KEY AUTO_INCREMENT,
+--    idInquilino INT,
+--    idVacina INT,
+--    FOREIGN KEY (idInquilino) REFERENCES inquilino (idInquilino),
+--    FOREIGN KEY (idVacina) REFERENCES Vacina (idVacina)
+--);
+
 #DROP TABLE AcessoArea;
 CREATE TABLE AcessoArea(
     idAcesso INT PRIMARY KEY AUTO_INCREMENT,
