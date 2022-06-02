20f76d7a26fa5c4a0263cebe2c85e09d7339e6f1
Erick Rodrigues Gomes - 822129827 <822129827@ulife.com.br>
2022-06-02 20:13:23 -0300

    Sql's

---
+++ SafePlace/src/Utils/SafePlace_Registros_Testes.sql
@@ -0,0 +1,5 @@
+INSERT INTO Condominio(nome,endereco) VALUES ('Com dó minio', 'Em baixo da tua mãe');
+INSERT INTO Area(descricao,dosesRequisitadas,idCondominio) VALUES ('Baile', 2, 1), ('Parquinho', 6, 1);
+
+INSERT INTO Condominio(nome,endereco) VALUES ('Predio gigante', 'Em baixo da teu pai');
+INSERT INTO Area(descricao,dosesRequisitadas,idCondominio) VALUES ('Hall principal', 2, 2), ('Sala de filmes', 6, 2);
