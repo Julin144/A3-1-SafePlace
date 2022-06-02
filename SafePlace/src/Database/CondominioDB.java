06ee33a9fd497f56672a71de274dab3c3c5571f4
Julio Assunção Lopes de Araujo - 822158274 <822158274@ulife.com.br>
2022-06-02 19:26:53 -0300

    Subir telas

--- SafePlace/src/Database/CondominioDB.java
+++ SafePlace/src/Database/CondominioDB.java
@@ -35,6 +35,8 @@ public class CondominioDB {
                 condominios[contador++] = new CondominioModel(id, nome, endereco);
             }
             return condominios;
+            
+            
         }
     }
     
--- SafePlace/src/View/TelaCadastroAcessoInquilinos.java
+++ SafePlace/src/View/TelaCadastroAcessoInquilinos.java
@@ -8,12 +8,12 @@ package View;
  *
  * @author Thais Dias
  */
-public class AcessoInquilinos extends javax.swing.JFrame {
+public class TelaCadastroAcessoInquilinos extends javax.swing.JFrame {
 
     /**
      * Creates new form AcessoInquilinos
      */
-    public AcessoInquilinos() {
+    public TelaCadastroAcessoInquilinos() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -124,20 +124,21 @@ public class AcessoInquilinos extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroAcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroAcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroAcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroAcessoInquilinos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AcessoInquilinos().setVisible(true);
+                new TelaCadastroAcessoInquilinos().setVisible(true);
             }
         });
     }
