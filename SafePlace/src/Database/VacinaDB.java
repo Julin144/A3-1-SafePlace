61777915672a88056b35f4b96e438fcc0200d7a8
Julio Assunção Lopes de Araujo - 822158274 <822158274@ulife.com.br>
2022-06-02 20:44:16 -0300

    subir db

---
+++ SafePlace/src/Database/VacinaDB.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+import Models.VacinaModel;
+/**
+ *
+ * @author 822158274
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class VacinaDB {
+    
+}
